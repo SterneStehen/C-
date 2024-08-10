5b5e3aef0a7c9621aa74cc4e84db8d2606838e74
SterneStehen <7353718@gmail.com>
2024-08-10 20:51:32 +0200

    ex01

--- С00/ex01/main.cpp
+++ С00/ex01/main.cpp
@@ -1,20 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 19:33:40 by smoreron          #+#    #+#             */
+/*   Updated: 2024/08/10 20:48:32 by smoreron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
 #include <iostream>
 
 int main()
 {
+	PhoneBook phoneBook;
+    std::string command;
 	std::string	cmd;
 	
 	while (true)
 	{
 		std::cout << "Enter command: \n" << std::endl;
 		std::cin >> cmd;
-		if(cmd == "ADD")
+		if(cmd == "ADD" || cmd == "add")
 		{
-
+			phoneBook.AddContact();
 		}
-		else if(cmd == "SEARCH")
+		else if(cmd == "SEARCH" || cmd == "search")
 		{
-
+			phoneBook.SearchContacts();
 		}
 		else if (cmd == "EXIT")
 		{
