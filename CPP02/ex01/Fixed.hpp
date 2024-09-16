a414880cadff19357a2e1c40e4ac6dad976559b8
SterneStehen <7353718@gmail.com>
2024-09-16 16:44:37 +0200

    add to int

--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,7 +6,34 @@
 /*   By: smoreron <7353718@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 15:29:58 by smoreron          #+#    #+#             */
-/*   Updated: 2024/09/16 15:29:59 by smoreron         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:44:32 by smoreron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int data;
+	const int bits = 8;
+public:
+	Fixed();
+	Fixed(int input);
+	Fixed(float input_float);
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other2);
+	
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+	void ft_print ();
+	~Fixed();
+};
+
+#endif
